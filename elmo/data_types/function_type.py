1c128e22f39f1aa0500c0a4167d9a8bdaa1812d5
Michael Brickman <mbricman8@gmail.com>
2018-02-23 19:46:51 +0200

    add an opcode class

--- elmo/data_types/function_type.py
+++ elmo/data_types/function_type.py
@@ -1,7 +1,8 @@
 from builtins import print
-from typing import Dict
+from typing import Dict, Union, List
 
 from elmo.data_types.base_data_type import BaseDataType
+from elmo.data_types.opcode_type import Opcode
 from .. import r2
 
 
@@ -19,30 +20,50 @@ class Function(BaseDataType):
         self._name = function_dict.get('name', 'unknown?')
         self.offset = function_dict.get('offset', 0)
 
-    def get_function_info(self):
+    def get_function_info(self) -> Dict[str, ]:
+        """
+        get the dictionary that represents all the info about this function
+        @return: a dict of info
+        """
         cmd = f"afij 0x{self.offset:x}"
+        print(cmd)
 
         function_infos = [f for f in self.cmdj(cmd) if f["offset"] == self.offset]
+
         if len(function_infos) > 1:
             raise Exception("Found multiple functions with in this offset")
+
         function_info = function_infos[0]
         return function_info
 
     @property
-    def name(self):
+    def name(self) -> str:
         function_info = self.get_function_info()
         return function_info['name']
 
     @name.setter
-    def name(self, new_name):
+    def name(self, new_name: str) -> None:
+        """
+        will set a new name to the function ( updating radare )
+        @param new_name: the new name for the function
+        """
         cmd = f"afn {new_name} @0x{self.offset:x}"
         self.cmd(cmd)
+
+    def opcodes(self) -> List[Opcode]:
+        """
+        @return: list of op code objects for the function
+        """
+        cmd = f"pdfj @0x{self.offset:x}"
+        opcodes_data = self.cmdj(cmd)['ops']
+        opcodes = [Opcode(op['offset'], _r2=self._r2) for op in opcodes_data]
+        return opcodes
 
     def print_opcodes(self):
         """
         print the function disassembled representation
         """
-        for opcode in self.cmdj('pdfj @ {}'.format(hex(self.offset)))['ops']:
-            if opcode['type'] == 'invalid':
+        for opcode in self.opcodes():
+            if not opcode.is_valid():
                 break
-            print(opcode['disasm'])
+            print(opcode.disasm())
--- elmo/data_types/opcode_type.py
+++ elmo/data_types/opcode_type.py
@@ -1,8 +1,29 @@
+from typing import Dict
+
 from elmo.data_types.base_data_type import BaseDataType
 
 
 class Opcode(BaseDataType):
 
-    def __init__(self, _r2):
-        super().__init__(_r2=_r2)
-        pass
+    def __init__(self, offset: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.offset = offset
+
+    def _get_opcode_data(self) -> Dict[str, ]:
+        cmd = f"pdj 1 @0x{self.offset:x}"
+        opcodes_data = self.cmdj(cmd)
+
+        if len(opcodes_data) > 1:
+            raise ValueError(f"Too many opcodes in at address 0x{self.offset}")
+
+        return opcodes_data[0]
+
+    def is_valid(self) -> bool:
+        return self._get_opcode_data().get('type') != 'invalid'
+
+    def type(self) -> str:
+        return self._get_opcode_data().get('type')
+
+    def disasm(self) -> str:
+        return self._get_opcode_data().get('disasm')
